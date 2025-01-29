31fd91d8a9926515e3636fbd3c6e910e6544f05a
amoora <omardiab.m@gmail.com>
2025-01-29 11:15:20 -0500

    spiderman

--- Lab4C.py
+++ Lab4C.py
@@ -2,7 +2,7 @@ side1 = int(input("Enter the first side of the triangle: "))
 side2 = int(input("Enter the second side of the triangle: "))
 side3 = int(input("Enter the third side of the triangle: "))
 
-if (side1 and side2 and side3) > 0 :
+if side1 >0 and side2 > 0 and side3 > 0 :
     if side1 == side2 == side3:
         print("The triangle is an equilateral triangle.")
     elif (side1 + side2 <= side3) or (side1 + side3 <= side2) or (side2 + side3 <= side1):
